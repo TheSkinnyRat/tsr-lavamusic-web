16b7d88b2910fede044b34d8d1f0c9b596168334
TheSkinnyRat <purwa.sabrang@gmail.com>
2022-07-30 04:36:48 +0700

    add now playing control button system

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -2,8 +2,15 @@ import CONFIG from './config';
 
 const ENDPOINTS = {
   MUSIC_SEARCH: (query) => `${CONFIG.BASE_URL}/music/search?query=${query}`,
-  GUILD_TRACK_POST: (guildId) => `${CONFIG.BASE_URL}/guild/${guildId}/track`,
-  GUILD_TRACK_RECOMMENDED_GET: (guildId) => `${CONFIG.BASE_URL}/guild/${guildId}/track/recommended`,
+  GUILD_TRACK: (guildId) => `${CONFIG.BASE_URL}/guild/${guildId}/track`,
+  GUILD_TRACK_RECOMMENDED: (guildId) => `${CONFIG.BASE_URL}/guild/${guildId}/track/recommended`,
+  GUILD_PAUSE: (guildId) => `${CONFIG.BASE_URL}/guild/${guildId}/pause`,
+  GUILD_UNPAUSE: (guildId) => `${CONFIG.BASE_URL}/guild/${guildId}/unpause`,
+  GUILD_NEXT: (guildId) => `${CONFIG.BASE_URL}/guild/${guildId}/next`,
+  GUILD_PREVIOUS: (guildId) => `${CONFIG.BASE_URL}/guild/${guildId}/previous`,
+  GUILD_LOOP: (guildId) => `${CONFIG.BASE_URL}/guild/${guildId}/loop`,
+  GUILD_UNLOOP: (guildId) => `${CONFIG.BASE_URL}/guild/${guildId}/unloop`,
+  GUILD_SHUFFLE: (guildId) => `${CONFIG.BASE_URL}/guild/${guildId}/shuffle`,
 }
 
 export default ENDPOINTS;
